a07bef00829330d70c265cf2504946c496de520c
mohamim360 <mohamim360@gmail.com>
2024-05-08 20:58:39 +0600

    db connention in queries

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,6 @@
 import { recipeModel } from "@/models/recipe-models";
 import { userModel } from "@/models/user-models";
+import { dbConnect } from "@/services/mongo";
 import {
   replaceMongoIdInArray,
   replaceMongoIdInObject,
@@ -7,21 +8,25 @@ import {
 import bcrypt from "bcrypt";
 
 async function getAllRecipes() {
+  await dbConnect();
   const allRecipes = await recipeModel.find().lean();
   return replaceMongoIdInArray(allRecipes);
 }
 
 async function getRecipeById(RecipeId) {
+  await dbConnect();
   const recipe = await recipeModel.findById(RecipeId).lean();
   return replaceMongoIdInObject(recipe);
 }
 
 async function getRecipeByCategory(RecipeCategory) {
+  await dbConnect();
   const recipes = await recipeModel.find({ category: RecipeCategory }).lean();
   return replaceMongoIdInArray(recipes);
 }
 
 async function createUser(user) {
+  await dbConnect();
   const { password, ...userData } = user;
   const hashedPassword = await bcrypt.hash(password, 10);
   const userWithHashedPassword = { ...userData, password: hashedPassword };
@@ -29,10 +34,14 @@ async function createUser(user) {
 }
 
 async function findUserByCredentials(credentials) {
-  const user = await userModel.findOne({email: credentials.email}).lean();
-console.log(user);
+  await dbConnect();
+  const user = await userModel.findOne({ email: credentials.email }).lean();
+  console.log(user);
   if (user) {
-    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
     if (isPasswordCorrect) {
       return replaceMongoIdInObject(user);
     }
@@ -41,6 +50,7 @@ console.log(user);
 }
 
 async function findUserById(id) {
+  await dbConnect();
   const user = await userModel.findOne(id).lean();
 
   if (user) {
@@ -49,23 +59,21 @@ async function findUserById(id) {
   return null;
 }
 
-
 async function updateFavourites(recipeId, authId) {
+  await dbConnect();
+  const user = await userModel.findById(authId);
 
-	const user = await userModel.findById(authId);
+  if (user) {
+    const foundFavourites = user.favourites.find((id) => id === recipeId);
 
-	if (user) {
-			const foundFavourites = user.favourites.find(id => id === recipeId);
+    if (foundFavourites) {
+      user.favourites.pull(recipeId);
+    } else {
+      user.favourites.push(recipeId);
+    }
 
-			if(foundFavourites) {
-					user.favourites.pull(recipeId);
-			} else {
-					user.favourites.push(recipeId);
-			}
-
-			user.save();
-	}
-
+    user.save();
+  }
 }
 
 export {
@@ -74,6 +82,6 @@ export {
   getRecipeByCategory,
   createUser,
   findUserByCredentials,
-	updateFavourites,
-	findUserById
+  updateFavourites,
+  findUserById,
 };
