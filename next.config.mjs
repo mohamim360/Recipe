3202069b82a8e6a3f760b291b65dac3401305c3f
mohamim360 <mohamim360@gmail.com>
2025-01-09 19:21:01 +0600

    img err 8

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-    domains: ["www.foodandwine.com","www.allrecipes.com","source.unsplash.com" ], 
+    domains: ["www.foodandwine.com","www.allrecipes.com","source.unsplash.com","www.averiecooks.com","cdn.loveandlemons.com" ], 
   },
 };
 
